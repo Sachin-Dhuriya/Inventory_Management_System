a3ee19143d50317f95c7918dc9a96b48bf3e3282
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2025-07-19 12:06:39 +0530

    Node Postgres connection Implimented

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const app = express();
 //-------------------------------------.ENV Setup----------------------------------------------------
 import dotenv from 'dotenv'
 dotenv.config()
+//--------------------------------------DATABASE Connection-------------------------------------------
+import pool from './config/db.js'
+
+(async()=>{
+    try {   
+        const res = await pool.query('SELECT NOW()');
+        console.log('Database connected successfully at: ', res.rows[0].now)
+        
+    } catch (err) {
+        console.error('Database Connection Failed...!!!', err)
+    }
+})();
+//-----------------------------------------------------------------------------------------------------------
+
+
+
+//--------------API-------------------
+
 
 
 
